21a66ea0a061320e532c4b068c31c1c02c5e6b72
Chen Nuode <chennuode2012@gmail.com>
2021-06-14 22:15:34 +0800

    Fixed binners

--- ZapQ/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-# Create your views here.
-
-class HelloView(APIView):
-    def get(self, request):
-        content = {'message': 'hello world'}
-        return Response(content)
